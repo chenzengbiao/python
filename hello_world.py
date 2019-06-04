cfb3c3f637a3a8d06683e04972eb791b072a1f07
chenzengbiao <clczb@163.com>
2019-06-04 05:08:42 -0700

    weekly-report@Week10,20190604

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+message = "Hello Python World"
+print(message)
+message = "Hello Python Crash Course World!"
+print(message)
+name = "ada lovelace"
+print(name.upper())
+print(name.title())
+name = "adA LoVElAce1"
+print(name.lower())
+# python
